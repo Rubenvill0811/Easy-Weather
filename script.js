43dc2bb9009f9e772462acc348ecc08247741e4d
Rubenvill0811 <152895707+Rubenvill0811@users.noreply.github.com>
2024-03-11 22:20:23 -0700

    fixed the logic
    I fixed the logic so that the search bar was working.

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const weatherApp = {
       this.getWeatherData();
   },
   
-  getWeatherData: function() {
+  getWeatherData: function(city) {
+    weatherApp.setupSearchForm();
+    if(city === undefined || city === null) {
+      return
+    }
       $.ajax({
-          url: "http://api.weatherapi.com/v1/forecast.json?key=1307cbf4acfb47bca2964553240903&q=Beaverton&days=5&aqi=no&alerts=no",
+          url: `http://api.weatherapi.com/v1/forecast.json?key=1307cbf4acfb47bca2964553240903&q=${city}&days=5&aqi=no&alerts=no`,
           method: 'GET',
           success: function(data) {
               window.weatherApp.renderWeatherData(data);
@@ -22,16 +26,17 @@ const weatherApp = {
       forecastDays.forEach(function(day) {
           const card = $('<div>').addClass('col').html(`<p>Date: ${day.date}</p><p>Temp: ${day.day.maxtemp_f}°F`);
           
-          container.append(card); // Append the card to the weather-container row
-      });
+          container.append(card);
+        });
   },
   setupSearchForm: function() {
     const form = $('#search-form');
-    
-    form.on('submit', function(event) {
+    console.log(form);
+    form.submit(function(event) {
         event.preventDefault();
         const city = $('#city-input').val();
         weatherApp.getWeatherData(city);
+        console.log(city);
     });
 }
 };
